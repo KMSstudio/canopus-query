d972925391cbb5182c5cddf465553b5d71cafa00
MS Kang <tomskang@naver.com>
2025-05-24 17:35:01 +0900

    fix: inquiry/__init__.py에 Literal 도입

--- inquiry/__init__.py
+++ inquiry/__init__.py
@@ -1,17 +1,18 @@
+from typing import Literal
 from .cj import cj_inquiry
 from .po import po_inquiry
 
 __all__ = ['cj_inquiry', 'po_inquiry', 'inquiry']
 
-def inquiry(company: str, invoice: str):
+def inquiry(company: Literal['cj', 'po'], invoice: str):
     """
-    company: 'cj' 또는 'po' 중 하나
+    company: 'cj' 또는 'po'만 가능
     invoice: 송장 번호
     """
-    company = company.lower()
     if company == 'cj':
         return cj_inquiry(invoice)
     elif company == 'po':
         return po_inquiry(invoice)
+
     else:
         raise ValueError("지원하지 않는 회사입니다. 'cj' 또는 'po'만 사용할 수 있습니다.")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from inquiry.cj import cj_inquiry
 from inquiry.po import po_inquiry
 
+invoice = "520315867871"  # 테스트용 송장번호
 data = cj_inquiry(invoice)
+data = po_inquiry("6896700517650")
 print(data)
