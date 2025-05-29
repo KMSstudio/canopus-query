91e95c9eeb4147a7205e2dfac92a0edf8a3f45e8
MS Kang <tomskang@naver.com>
2025-05-29 19:41:50 +0900

    make inquiry function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from inquiry.cj import cj_inquiry
 from inquiry.po import po_inquiry
+from inquiry import inquiry
 
 invoice = "520315867871"  # 테스트용 송장번호
-data = cj_inquiry(invoice)
-data = po_inquiry("6896700517650")
+data = inquiry('cj', invoice)
+data = inquiry('po', "6896700517650")
 print(data)
